2f894f6de82dc26b9269e06a17e9b42a5ee4a7b3
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-09 13:53:16 +0200

    added needed assets and started work on channel list container

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,3 +1,22 @@
+import { ChannelList, useChatContext } from "stream-chat-react";
+// import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import Cookies from 'universal-cookie';
+import HospitalIcon from '../assets/hospital.png';
+
+const SideBar = () => {
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={HospitalIcon} alt="Hospital" width="30" />
+
+            </div>
+
+        </div>
+
+    </div>
+}
+
+
 const ChannelListContainer = () => {
     return (
         <div>
