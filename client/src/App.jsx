155daf12596214282cddf58577601db4649c0f4c
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-09 12:34:04 +0200

    created a service for importing components for easier management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,8 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import  Cookies  from 'universal-cookie';
 import './App.css'
-import ChannelListContainer from './components/ChannelListContainer';
-import ChannelContainer from './components/ChannelContainer';
 const apiKey = 'tqhygffd5zvz';
-
+import { ChannelListContainer, ChannelContainer } from './components';
 const client = StreamChat.getInstance(apiKey);
 
 function App() {
